dda9695cc387659e3d4c1095641e7fc166cc85e9
stphn <stephanie1117@hotmail.es>
2020-07-23 00:22:45 -0600

    yml

--- DBServer/test-mongo-app/src/connection.js
+++ DBServer/test-mongo-app/src/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const User = require("./User.model");const connection = "mongodb://localhost:27017/mongo-test";
+const User = require("./User.model");
+const connection = "mongodb://mongo:27017/mongo-test";
 const connectDb = () => {
  return mongoose.connect(connection);
 };
